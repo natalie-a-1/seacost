24cc03d23cd29afe58ed475d9c32e91c9429dc41
nataliehill <natalie.a.hill-1@ou.edu>
2024-10-07 16:19:49 -0500

    Edit svg

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../images/Web-developer.svg';
+import img from '../images/design.svg';
 import { Link } from 'react-router-dom';
 
 const Intro = () => {
